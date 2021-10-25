ae6cf2840af93b42402caa7e8d02fb5c6c9d7a74
GianlucaPolcino <gianluca.polcino.gp@gmail.com>
2021-10-25 14:48:25 +0200

    project done

--- assets/scypt.js
+++ assets/scypt.js
@@ -6,4 +6,15 @@ for (let i = 1; i <= 100; i++){
     box.innerHTML = i;
     row.append (box);
     console.log(box);
+
+    if (!(i % 5) && !(i % 3)){
+        box.classList.add('gp-fifteen');
+        box.innerHTML = 'fizzbuzz';
+    } else if (!(i % 5)){
+        box.classList.add('gp-five');
+        box.innerHTML = 'buzz';
+    } else if (!(i % 3)){
+        box.classList.add ('gp-three');
+        box.innerHTML = 'fizz';
+    }
 }
